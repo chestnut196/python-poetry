ee94f4c13dbba14959125e2951599883d8bddfe9
My Name <me@example.com>
2025-07-03 16:26:27 +0200

    34

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e  # Exit on any error
+
+echo "Installing poetry..."
+curl -sSL https://install.python-poetry.org | python3
+
+echo "Export path..."
+export PATH=\"$HOME/.local/bin:$PATH\"
+
+echo "Running poetry install..."
+poetry install
+
+echo "Setup complete!"
